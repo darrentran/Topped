4a1ea9a589321f03f4979214ba1792521f19668d
Henry Wee <wjxhenry@gmail.com>
2020-01-27 00:31:22 -0700

    Add Firebase (#2)
    * Add Firebase
    
    * TODO

---
+++ src/Firebase/Database/firebase.tsx
@@ -0,0 +1,100 @@
+import app from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import Competition from "../../models/competition";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+};
+const COMPETITIONS_COLLECTION = "competitions";
+
+class Firebase {
+
+    // TODO: Fix types
+    auth: any;
+    firestore: any;
+
+    constructor() {
+        app.initializeApp(firebaseConfig);
+
+        this.auth = app.auth();
+        this.firestore = app.firestore();
+    }
+
+    /** TODO: Temporary test function -> Remove */
+    testCallback = (result: string) => {
+        console.log(result);
+    }
+
+    doCreateUserWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email: string, password: string) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = (email: string) => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = (password: string) =>
+        this.auth.currentUser.updatePassword(password);
+
+    /**
+     * Creates a new competition entry or updates an existing one
+     * @param competition The competition object to create/update
+     */
+    doInsertCompetition = (competition: Competition) => {
+        let competitionRef = this.firestore.collection(COMPETITIONS_COLLECTION).doc();
+        competitionRef.set({
+            ...competition,
+            id: competitionRef.id
+        });
+    }
+
+    /**
+     * Updates the competition object from firebase
+     * @param competition
+     */
+    doUpdateCompetition = (competition: Competition) => {
+        let competitionRef = this.firestore.collection(COMPETITIONS_COLLECTION).doc(competition.id);
+        competitionRef.set(competition);
+    }
+
+    /**
+     * Deletes the competition object from firebase
+     * @param competition The competition object to delete
+     */
+    doDeleteCompetition = (competition: Competition) => {
+        let competitionRef = this.firestore.collection(COMPETITIONS_COLLECTION).doc(competition.id);
+        competitionRef.delete();
+    }
+
+    /**
+     * Gets all the documentation in the specified competitions collection
+     * @param callback The callback when the competitions collection is updated (returns the querySnapshot). Obtain
+     * data by iterating through snapshot and calling .data().
+     */
+    receiveCompetitionUpdates = (callback: (querySnapshot: any /** TODO: Fix type */) => void) => {
+        let query = this.firestore.collection(COMPETITIONS_COLLECTION);
+        query.onSnapshot((querySnapshot: any /** TODO: Fix type */) => {
+            // console.log(querySnapshot)
+            console.log(`Received query snapshot of size ${querySnapshot.size}`);
+            // querySnapshot.forEach(doc => {
+            //     console.log(doc.data())
+            // })
+            callback(querySnapshot);
+        }, (err: Error) => {
+            console.log(`Encountered error: ${err}`);
+            callback(null);
+        })
+    }
+}
+
+  
+export default Firebase;
---
+++ src/models/competition.ts
@@ -0,0 +1,32 @@
+var currentDate = new Date().toJSON();
+var statusEnum = Object.freeze({"active":1, "completed":2, "not_started":3})
+
+export default class Competition {
+
+        id: string;
+        name: string;
+        desc: string;
+        startDate: string;
+        endDate: string;
+        fee: string;
+        problems: string;
+        status: number;
+        admins: string;
+
+
+    constructor(id: string, name: string, desc: string, startDate: string, endDate: string, problems: string, fee: string, admins: string) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.fee = fee;
+        this.problems = problems;
+        if (currentDate > this.startDate && currentDate < this.endDate) {
+            this.status = statusEnum.active;
+        } else {
+            this.status = statusEnum.not_started;
+        }
+        this.admins = admins;
+    }
+  }
